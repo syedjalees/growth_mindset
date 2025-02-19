c47f3173f56c680461b68cc6898e46a1f2058f02
SYED JALEES AKBAR SHAH <sayyedjalees958@gmail.com>
2025-02-20 03:17:33 +0500

    new

--- growth.py
+++ growth.py
@@ -30,7 +30,8 @@ if uploaded_files:
             if file_ext == ".csv":
                 df = pd.read_csv(file)
             elif file_ext in [".xlsx", ".xls"]:
-                df = pd.read_excel(file, engine="openpyxl")
+                df = pd.read_excel(file, engine='openpyxl')
+
             else:
                 st.error(f"Unsupported file type: {file_ext}")
                 continue
