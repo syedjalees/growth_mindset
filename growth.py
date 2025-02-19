0ad38648dde58cd06bcdfee501da63003673db95
SYED JALEES AKBAR SHAH <sayyedjalees958@gmail.com>
2025-02-19 21:03:11 +0500

    update again

--- growth.py
+++ growth.py
@@ -60,7 +60,7 @@ if uploaded_files:
                     
             with col2:
                 if st.button(f"Fill missing values for : {file.name}"):
-                    numeric_cols = df.select_dtypes(includes=['number']).columns
+                    numeric_cols = df.select_dtypes(include=['number']).columns
                     df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
                     st.write("✅ Missing values filled successfully!")
     
