fc30728419381ae5e08f455b78d93398e7e4e3f5
SYED JALEES AKBAR SHAH <sayyedjalees958@gmail.com>
2025-02-19 20:31:25 +0500

    Initial commit - Growth Mindset Streamlit Project

---
+++ growth.py
@@ -0,0 +1,102 @@
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
+
+st.set_page_config(page_title="Data Sweeper", layout="wide")
+
+
+#custom css
+st.markdown("""
+<style>
+.stApp {
+    background-color: black;
+    color:white;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
+
+#title and description
+st.title("Data Sweeper Sterling Integrator By Sayyed Jalees")
+st.write("Upload your CSV file and select the columns you want to keep. The app will remove the columns you don't want and save the new file.")
+st.write("Transform your files between CSV and Excel formats with built-in data cleaning and visualization Creating the project for Quarter 3!")
+
+
+#file uploader
+uploaded_files = st.file_uploader("Upload your files (accepts CSV and Excel files) : ", type=["csv", "xlsx", "xls"], accept_multiple_files=True)
+
+if uploaded_files:
+    for file in uploaded_files:
+        file_ext = os.path.splitext(file.name)[-1].lower()
+        
+        if file_ext == ".csv":
+            df = pd.read_csv(file) 
+        elif file_ext == "xlsx" :
+            df = pd.read_excel(file)
+        elif file_ext == "xls" :
+            df = pd.read_excel(file)
+        else:
+            st.error(f"unsupported file type {file_ext}")
+            continue
+        
+        #file details 
+        st.write("🔍 Preview the head of the Dataframe")
+        st.dataframe(df.head())
+        
+        
+        
+         
+        #Data Cleaning Options
+        st.subheader("🛠️ Data Cleaning Options")
+        if st.checkbox(f"Clean data for {file.name}"):
+            col1, col2 = st.columns(2)
+            
+            with col1:
+                if st.button(f"Remove Duplicates for : {file.name}"):
+                    df.drop_duplicates(inplace=True)
+                    st.write("✅ Duplicates removed successfully!")
+                    
+            with col2:
+                if st.button(f"Fill missing values for : {file.name}"):
+                    numeric_cols = df.select_dtypes(includes=['number']).columns
+                    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+                    st.write("✅ Missing values filled successfully!")
+    
+        st.subheader("🎯 Select Columns to Keep")
+        columns = st.multiselect(f"Select columns to keep for {file.name}", df.columns, default=df.columns)
+        df = df[columns]
+        
+        
+        #data visualization
+        st.subheader("📊 Data Visualization")
+        if st.checkbox(f"Visualize data for {file.name}"):
+            st.bar_chart(df.select_dtypes(include='number').iloc[:, :2])
+            
+            
+        #Conversion Options
+        st.subheader("🔄 Conversion Options")
+        conversion_type = st.radio(f"Select conversion type for {file.name} to : ", ["CSV", "Excel"], key=file.name)
+        if st.button(f"Convert {file.name}"):
+            buffer = BytesIO() 
+            if conversion_type == "CSV":
+                df.to_csv(buffer, index=False)
+                file_name = file.name.replace(file_ext, ".csv")
+                mime_type = "text/csv"
+                
+            elif conversion_type == "Excel":
+                df.to_excel(buffer, index=False)
+                file_name = file.name.replace(file_ext, ".xlsx")
+                mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            buffer.seek(0)
+            
+            
+            st.download_button(
+                label=f"Download {file_name} as {conversion_type}", 
+                data=buffer, 
+                file_name=file_name, 
+                mime=mime_type
+            )
+            
+st.success("🎉 All files processed successfully!")
